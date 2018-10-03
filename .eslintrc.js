e615fb68a683d8002fe85cc1098d0a725ac84fe3
Stuart Lees <stuartlees@gmail.com>
2018-10-03 18:05:09 -0400

    Add infrastructure config files

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "idiomatic"
+  ],
+  "rules": {
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "newline-after-var": [
+      "error",
+      "always"
+    ]
+  }
+};
