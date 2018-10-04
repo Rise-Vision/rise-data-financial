50e1a97d003eb57733a68efb9f7ff13aa4e057f3
Stuart Lees <stuartlees@gmail.com>
2018-10-04 17:14:37 -0400

    Add missing constructor to component source

--- src/rise-data-financial.js
+++ src/rise-data-financial.js
@@ -1,6 +1,13 @@
 import { PolymerElement } from "@polymer/polymer/polymer-element.js";
 
 class RiseDataFinancial extends PolymerElement {
+
+  constructor() {
+    super();
+
+    // TODO: coming soon
+  }
+
 }
 
 customElements.define( "rise-data-financial", RiseDataFinancial );
